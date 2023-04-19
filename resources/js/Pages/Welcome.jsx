356b29fd9f3d918a861ac268a22e850e67c8382a
nitya <nando@pixelsmith.co>
2023-04-19 15:34:09 -0700

    init

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,48 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Welcome(props) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                    {props.auth.user ? (
+                        <Link
+                            href={route('dashboard')}
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route('login')}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route('register')}
+                                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+
+                <section className="relative min-h-screen w-full">
+                    <figure className="absolute top-0 flex min-h-screen">
+                        <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" className="min-h-screen object-cover" alt="" />
+                    </figure>
+                    <div className="absolute">
+                        <h1 className="title color-white">Showroom Ready</h1>
+                    </div>
+                </section>
+            </div>
+
+          
+        </>
+    );
+}
